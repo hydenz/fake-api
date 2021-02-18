b71b73297670e1936f904778023bd024fa236528
Marcelo Figueiredo de Lima <marcelof_lima@hotmail.com>
2021-02-18 12:10:13 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,8 @@ app.get('/api/:id', cors(), (req, res) => {
 
 // POST new JSON
 app.post('/api/', cors(customCors), (req, res) => {
+    const contentType = response.headers.get("content-type");
+    if (contentType !== "application/json") return res.send({msg: "Apenas o formato JSON é suportado}")
     let json = JSON.stringify(req.body)
     knex('jsons').insert({ json }, "id").then(value => {
         let id = value[0]
