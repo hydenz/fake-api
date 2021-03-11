ab026bc94d5a4848e3f494d0cee38dd828f997e0
hydenz <marcelof_lima@hotmail.com>
2021-03-10 23:06:15 -0300

    Added cors to all routes, added builtin express json parser, fixed error handling, removed path import

--- frontend/src/Components/Api.jsx
+++ frontend/src/Components/Api.jsx
@@ -1,68 +1,83 @@
-import React, { useState, useEffect } from 'react'
-import Container from "react-bootstrap/Container"
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-import Card from "react-bootstrap/Card"
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import axios from 'axios';
 
 const Api = () => {
-    const [endPoint, setendPoint] = useState("")
-    const [output, setOutput] = useState("")
-    useEffect(() => {
-        let endpoint = localStorage.getItem('endpoint')
-        endpoint && setendPoint(endpoint)
-    }, [])
+  const [endPoint, setendPoint] = useState('');
+  const [output, setOutput] = useState('');
+  useEffect(() => {
+    let endpoint = localStorage.getItem('endpoint');
+    endpoint && setendPoint(endpoint);
+  }, []);
 
-    const getJson = e => {
-        e.preventDefault()
-        axios.get(endPoint).then(resp => {
-            resp.data = JSON.stringify(resp.data, null, 2)
-            setOutput(resp.data)
-        }).catch(err => {
-            if (err.response) {
-                setOutput(`Unable to find JSON of ID ${endPoint.split("/").pop()}`)
-            } else if (err.request) {
-                setOutput("Server out of reach, please try again later")
-            }
-        })
-    }
-    return (
-        <Container>
-            <Row className="mt-5">
-                <Col>
-                    <Card className="text-center">
-                        <Card.Header>Use the API</Card.Header>
-                        <Card.Body className="text-left">
-                            <Form>
-                                <Form.Row>
-                                    <Form.Label column="lg" xs="auto">Endpoint:</Form.Label>
-                                    <Col xs={true}>
-                                        <Form.Control size="lg" type="text" value={endPoint}
-                                            onChange={e => setendPoint(e.target.value)} />
-                                    </Col>
-                                    <Col xs="auto">
-                                        <Button variant="success" size="lg" onClick={getJson}>
-                                            GET
-                                            </Button>
-                                    </Col>
-                                </Form.Row>
-                                <Form.Row>
-                                    <Form.Text className="text-muted ml-1 mt-2">
-                                        {`You can use our example endpoint ${window.location.href}/1`}
-                                    </Form.Text>
-                                </Form.Row>
-                            </Form>
-                            <h2 className="mt-2">Response:</h2>
-                            <Form.Control as="textarea" rows={12} style={{ resize: "none" }} spellCheck="false"
-                                readOnly={true} value={output} />
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+  const getJson = (e) => {
+    e.preventDefault();
+    axios
+      .get(endPoint)
+      .then((resp) => {
+        resp.data = JSON.stringify(resp.data, null, 2);
+        setOutput(resp.data);
+      })
+      .catch((err) => {
+        if (err.response) {
+          setOutput(err.response.data.msg);
+        } else if (err.request) {
+          setOutput('Server out of reach, please try again later');
+        }
+      });
+  };
+  return (
+    <Container>
+      <Row className='mt-5'>
+        <Col>
+          <Card className='text-center'>
+            <Card.Header>Use the API</Card.Header>
+            <Card.Body className='text-left'>
+              <Form>
+                <Form.Row>
+                  <Form.Label column='lg' xs='auto'>
+                    Endpoint:
+                  </Form.Label>
+                  <Col xs={true}>
+                    <Form.Control
+                      size='lg'
+                      type='text'
+                      value={endPoint}
+                      onChange={(e) => setendPoint(e.target.value)}
+                    />
+                  </Col>
+                  <Col xs='auto'>
+                    <Button variant='success' size='lg' onClick={getJson}>
+                      GET
+                    </Button>
+                  </Col>
+                </Form.Row>
+                <Form.Row>
+                  <Form.Text className='text-muted ml-1 mt-2'>
+                    {`You can use our example endpoint ${window.location.href}/1`}
+                  </Form.Text>
+                </Form.Row>
+              </Form>
+              <h2 className='mt-2'>Response:</h2>
+              <Form.Control
+                as='textarea'
+                rows={12}
+                style={{ resize: 'none' }}
+                spellCheck='false'
+                readOnly={true}
+                value={output}
+              />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Api
+export default Api;
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,85 +1,121 @@
-import React, { useState, useEffect } from 'react'
-import Container from "react-bootstrap/Container"
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-import Card from "react-bootstrap/Card"
-import Modal from "react-bootstrap/Modal"
-import axios from "axios"
-
+import React, { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
 
 const Home = () => {
-    const apiUrl = window.location.href.replace("home", "api/")
-    const [userInput, setuserInput] = useState("")
-    const [validJson, setvalidJson] = useState({ text: "", color: "", disabledBtn: true })
-    const [modalOpts, setmodalOpts] = useState({ show: false, msg: "" })
-    const postJson = e => {
-        e.preventDefault()
-        let json = JSON.parse(userInput)
-        axios.post(apiUrl, json).then(res => {
-            setmodalOpts({ show: true, msg: res.data.msg })
-            let endpoint = res.config.url + res.data.id
-            localStorage.setItem("endpoint", endpoint)
-        }).catch(reason => setmodalOpts({
-            show: true,
-            msg: "There was an error processing your request, please try again later"}))
+  const [userInput, setuserInput] = useState('');
+  const [validJson, setvalidJson] = useState({
+    text: '',
+    color: '',
+    disabledBtn: true,
+  });
+  const [modalOpts, setmodalOpts] = useState({ show: false, msg: '' });
+  const postJson = (e) => {
+    e.preventDefault();
+    let json = JSON.parse(userInput);
+    axios
+      .post('/api', json)
+      .then((res) => {
+        setmodalOpts({ show: true, msg: res.data.msg });
+        let endpoint = res.config.url + res.data.id;
+        localStorage.setItem('endpoint', endpoint);
+      })
+      .catch(() =>
+        setmodalOpts({
+          show: true,
+          msg:
+            'There was an error processing your request, please try again later',
+        })
+      );
+  };
+  useEffect(() => {
+    if (userInput) {
+      try {
+        JSON.parse(userInput);
+        setvalidJson({
+          text: 'Valid JSON Format',
+          color: 'success',
+          disabledBtn: false,
+        });
+      } catch (err) {
+        setvalidJson({
+          text: 'Invalid JSON Format',
+          color: 'danger',
+          disabledBtn: true,
+        });
+      }
     }
-    useEffect(() => {
-        if (userInput) {
-            try {
-                JSON.parse(userInput)
-                setvalidJson({ text: "Valid JSON Format", color: "success", disabledBtn: false })
-            }
-            catch (err) {
-                setvalidJson({ text: "Invalid JSON Format", color: "danger", disabledBtn: true })
-            }
-        }
-    }, [userInput])
-    return (
-        <Container>
-            <Row className="mt-5">
-                <Col>
-                    <Card className="text-center">
-                        <Card.Header>Post Your JSON</Card.Header>
-                        <Card.Body>
-                            <Card.Title>Enter a JSON text and we will generate an API to handle it</Card.Title>
-                            <Form className="mt-4">
-                                <Form.Group controlId="formBasicText">
-                                    <Form.Control as="textarea" rows={12} style={{ resize: "none" }} spellCheck="false"
-                                        value={userInput} onChange={e => setuserInput(e.target.value)} />
-                                </Form.Group>
-                                <Container>
-                                    <Row>
-                                        <Col>
-                                            <p className={`text-left text-${validJson.color}`}>
-                                                {validJson.text}
-                                            </p>
-                                        </Col>
-                                        <Col className="text-right">
-                                            <Button variant="primary" type="submit" disabled={validJson.disabledBtn}
-                                                onClick={postJson}>Submit</Button>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Modal show={modalOpts.show} onHide={() => setmodalOpts({ show: false, msg: "" })}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Request Response</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{modalOpts.msg}</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => setmodalOpts({ show: false, msg: "" })}>
-                        Close
+  }, [userInput]);
+  return (
+    <Container>
+      <Row className='mt-5'>
+        <Col>
+          <Card className='text-center'>
+            <Card.Header>Post Your JSON</Card.Header>
+            <Card.Body>
+              <Card.Title>
+                Enter a JSON text and we will generate an endpoint to GET it
+              </Card.Title>
+              <Form className='mt-4'>
+                <Form.Group controlId='formBasicText'>
+                  <Form.Control
+                    as='textarea'
+                    rows={12}
+                    style={{ resize: 'none' }}
+                    spellCheck='false'
+                    value={userInput}
+                    onChange={(e) => setuserInput(e.target.value)}
+                  />
+                </Form.Group>
+                <Container>
+                  <Row>
+                    <Col>
+                      <p className={`text-left text-${validJson.color}`}>
+                        {validJson.text}
+                      </p>
+                    </Col>
+                    <Col className='text-right'>
+                      <Button
+                        variant='primary'
+                        type='submit'
+                        disabled={validJson.disabledBtn}
+                        onClick={postJson}
+                      >
+                        Submit
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Modal
+        show={modalOpts.show}
+        onHide={() => setmodalOpts({ show: false, msg: '' })}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Request Response</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{modalOpts.msg}</Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant='secondary'
+            onClick={() => setmodalOpts({ show: false, msg: '' })}
+          >
+            Close
           </Button>
-                </Modal.Footer>
-            </Modal>
-        </Container>
-    )
-}
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
